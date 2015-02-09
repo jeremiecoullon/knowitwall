b8d2fb9a1da485ecc07b4cf9997fa3ffbbd4502a
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-02-09 21:36:41 +0000

    got partial content to work! its streaming, but going back to home page doesnt work (broken pipe stuff). maybe if its up itll work fine..

--- app.py
+++ app.py
@@ -1,9 +1,79 @@
 import os
+import re
+import mimetypes
 from flask import Flask, json
-from flask import render_template, request, send_from_directory
+from flask import render_template, request, send_from_directory, Response
 
 app = Flask(__name__, static_folder='static')
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Accept-Ranges', 'bytes')
+    return response
+
+
+
+"----------------------------------------------------------------------------------------------------"
+"send_file_partial function"
+
+
+
+def send_file_partial(path):
+    """
+        Simple wrapper around send_file which handles HTTP 206 Partial Content
+        (byte ranges)
+        TODO: handle all send_file args, mirror send_file's error handling
+        (if it has any)
+    """
+    range_header = request.headers.get('Range', None)
+    if not range_header: return send_from_directory(app.static_folder, path)
+
+    new_path=os.path.join('static/',path)
+    print new_path
+
+
+    size = os.path.getsize(new_path)
+    byte1, byte2 = 0, None
+
+    m = re.search('(\d+)-(\d*)', range_header)
+    g = m.groups()
+
+    if g[0]: byte1 = int(g[0])
+    if g[1]: byte2 = int(g[1])
+
+    length = size - byte1
+    if byte2 is not None:
+        length = byte2 - byte1+1
+
+    data = None
+    with open(new_path, 'rb') as f:
+        f.seek(byte1)
+        data = f.read(length)
+
+    rv = Response(data,
+        206,
+        mimetype=mimetypes.guess_type(new_path)[0],
+        direct_passthrough=True)
+    rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+
+    return rv
+
+
+
+"----------------------------------------------------------------------------------------------------"
+"sending files partially"
+
+
+@app.route('/audio/science_ganymede.mp3')
+@app.route('/ganymede_transcript.txt')
+@app.route('/Mehdi.jpg')
+def static_from_root():
+    return send_file_partial(request.path[1:])
+
 
 
 "----------------------------------------------------------------------------------------------------"
@@ -17,13 +87,6 @@ def index():
     This method is a controller. a method is function with side-effects. python just has methods.
     """
     return render_template('/index/index.html')
-
-
-@app.route('/audio/science_ganymede.mp3')
-@app.route('/ganymede_transcript.txt')
-@app.route('/Mehdi.jpg')
-def static_from_root():
-    return send_from_directory(app.static_folder, request.path[1:])
 
 
 
@@ -60,11 +123,9 @@ def ad1():
     with open(bio_path, "r") as f:
         author_bio = f.read()
 
-    return render_template('audio_doc.html', transcript=transcript)
-    """
-    , transcript=transcript, author_image=ad.author_image, topic_image=ad.topic_image,
+    return render_template('audio_doc.jade', transcript=transcript, author_image=ad.author_image, topic_image=ad.topic_image,
         author_bio=author_bio, author_name=ad.author_name, audio=ad.audio, discipline=ad.discipline, form=ad.form, bige=bige)
-    """
+
 
 
 "----------------------------------------------------------------------------------------------------"
