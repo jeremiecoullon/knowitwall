87d9dc0d60cceb9fe1d825a8235284591d998804
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-04-17 16:41:58 +0100

    testing the random bit i added to app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import render_template, request, send_from_directory, Response
 app = Flask(__name__, static_folder='static')
 
 
-lala= 'testing branches'
 
 """----------------------------------------------------------------------------------------------------
 To change audio-doc:
