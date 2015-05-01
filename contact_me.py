e99ab2775267d5cedf8ce7c7eae47524795fec3f
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-05-01 21:12:10 +0100

    working on contact form. need to get contact_me.py to read ajax post variables

--- contact_me.py
+++ contact_me.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import yagmail
+from flask import requests
+
+print request.GET['name']
 
 # send email to theknowitwall@gmail.com
-yagmail.Connect('emailtoknowitwall', 'startupsarefun').send('theknowitwall@gmail.com', 'hello!', 'this is the body')
+#yagmail.Connect('emailtoknowitwall', 'startupsarefun').send('theknowitwall@gmail.com', 'hello!', 'this is the body')
