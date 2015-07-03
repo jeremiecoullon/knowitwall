f1f9f8c9bc01ea86732ceb9dfbc8541ba18a3b13
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-07-03 18:51:06 +0100

    put a seperate page for audiodoc. looks allrigh

--- app.py
+++ app.py
@@ -107,6 +107,31 @@ def index():
 than define the Struct class. the second option is safer (if the key doesnt exist) """
 
 
+"----------------------------------------------------------------------------------------------------"
+" audiodocs on seperate page "
+
+@app.route('/audiodoc')
+def audiodoc():
+
+    audiodoc_list = ['ganymede.json', 'tate.json']
+    audiodocs =[]
+    for audiodoc_json in audiodoc_list:
+
+        with open('json_files/' + audiodoc_json, "r") as json_file:
+            ad_dictionary = json.load(json_file)
+
+        with open(ad_dictionary.get('author_bio'), "r") as f:
+            author_bio = f.read().decode('utf-8')
+        ad_dictionary['author_bio'] = author_bio
+
+        with open(ad_dictionary.get('transcript'), "r") as f:
+            transcript = f.read().decode('utf-8')
+        ad_dictionary['transcript'] = transcript
+
+        audiodocs.append(ad_dictionary.copy())
+
+    return render_template('audiodoc.html', audiodocs=audiodocs)
+
 
 "----------------------------------------------------------------------------------------------------"
 "method for AJAX contact form "
