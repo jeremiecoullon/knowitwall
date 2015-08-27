874b2edd171f376dea25720b9d5f3bebb132a27b
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-08-27 19:46:09 +0100

    a

--- app/views.py
+++ app/views.py
@@ -78,7 +78,7 @@ def static_from_root():
 "audiodoc function"
 
 # complete list of audiodocs
-all_audiodocs = ['antartica_discovery.json', 'stegosaurus.json', 'ganymede.json', 'tate.json']
+all_audiodocs = ['memory_bike.json', 'antartica_discovery.json', 'stegosaurus.json', 'ganymede.json', 'tate.json']
 
 # input list of json files, outputs list of dictionaries of variables paths & unicode to pass to templates
 def ad_fun(audiodoc_list):
