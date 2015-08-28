7beff6f5ea3ca47bee84dc41671715b22f635c11
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-08-28 18:50:00 +0100

    working through part 4 of miguels tutorial. just created models.py. Im at the Creating the database section

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# login stuff
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -7,3 +11,7 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+# SQLalchemy stuff
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
