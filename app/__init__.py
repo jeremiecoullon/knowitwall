4b55cc81ad025c87a13b70f8c89a06eb76824e67
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-08-30 17:02:11 +0100

    finished miguels tutorial (part5: user logins). now going to use OAuth for login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__, static_folder='static')
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
+
 
 
 from app import views, models
