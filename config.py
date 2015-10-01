9ba6ecea39534b97656c8162a4f5beec020e6ae1
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2015-10-01 19:55:34 +0100

    trying to figure out annotation permissions..

--- config.py
+++ config.py
@@ -16,15 +16,6 @@ OAUTH_CREDENTIALS = {
         'secret': 'lUjEtyqRIb1d5Le0MruxQZGJIzj7eaLxN5jA8hc0rGNM6mgIyL'
     }}
 
-
-
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
-
 # SQLalchemy stuff
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
