1891846590a80aeb61e219f8aba7857a5b86b4e7
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2016-01-28 19:56:17 +0000

    circular player is tripping out; different divs are flying all over the place..

--- app/static/js/circular_player.js
+++ app/static/js/circular_player.js
@@ -1,6 +1,5 @@
-
-var audio = document.getElementById('audio');
-var progress = document.getElementById('progress');
+var audio = document.getElementById('circular-audio');
+var progress = document.getElementById('circular-progress');
 var playpause = document.getElementById("play-pause");
 var volume = document.getElementById("volume");
 
@@ -29,7 +28,7 @@ function setVolume() {
 function updateProgress() {
 	var percent = Math.floor((100 / audio.duration) * audio.currentTime);
 	progress.value = percent;
-	var canvas = document.getElementById('progress');
+	var canvas = document.getElementById('circular-progress');
 	var context = canvas.getContext('2d');
 	var centerX = canvas.width / 2;
 	var centerY = canvas.height / 2;
@@ -40,18 +39,27 @@ function updateProgress() {
 	context.beginPath();
 	context.arc(centerX, centerY, radius, 0, ((circ) * cpercent), false);
 	context.lineWidth = 10;
-	context.strokeStyle = 'rgba(255, 194, 28, 0.8)';
+	context.strokeStyle = '#ffc21c';
 	context.stroke();
 	if (audio.ended) resetPlayer();
 }
 
 function resetPlayer() {
-	  audio.currentTime = 0;
-    context.clearRect(0,0,canvas.width,canvas.height);
-    playpause.title = "Play";
+	  audio.currentTime = 0; context.clearRect(0,0,canvas.width,canvas.height);
+  playpause.title = "Play";
 	  playpause.innerHTML = '<i class="fa fa-play fa-3x"></i>';
 }
 
+// thx to: http://www.adobe.com/devnet/html5/articles/html5-multimedia-pt3.html
+
+// Circular rotating music player with menu on back with html5 javascript and css3 is  MIT licensed:  http://opensource.org/licenses/MIT
+// modified for Knowitwall 2016
+
+
+function setLocation(){
+    var percentage = 0.80;
+    audio.currentTime = audio.duration * percentage;
+}
 
 
 // Circular rotating music player with menu on back with html5 javascript and css3 is  MIT licensed:  http://opensource.org/licenses/MIT
