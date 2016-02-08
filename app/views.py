2b1899aa5204a09f4cfb41e1d2548d53ef8dc593
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2016-02-08 10:25:21 +0000

    language audiodoc is ready to put up

--- app/views.py
+++ app/views.py
@@ -74,6 +74,7 @@ def send_file_partial(path):
 @app.route('/audio/neural_cartography.mp3')
 @app.route('/audio/real_shakespeare.mp3')
 @app.route('/audio/higgs_boson.mp3')
+@app.route('/audio/human_language.mp3')
 def static_from_root():
     return send_file_partial(request.path[1:])
 
