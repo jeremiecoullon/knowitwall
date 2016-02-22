9337392a14e7b608a12647f762614db6a0023ede
Ian Jones <ianjones227@users.noreply.github.com>
2016-02-22 00:12:22 +0000

    Added progress control for circular player.

--- test circular player/test circular player/circular_player.js
+++ test circular player/test circular player/circular_player.js
@@ -4,11 +4,52 @@ var progress = document.getElementById('progress');
 var playpause = document.getElementById("play-pause");
 var volume = document.getElementById("volume");
 
+//how far from 3 o'clock the progress bar starts, 0 - 2*pi
+var progressOffsetRadians = 147 * (Math.PI / 180);
+//the distance from the centre of the player to the middle of the progress bar
+var progressRadius = 150;
+//the thickness of the progress bar
+var progressWidth = 10;
+//how far from the middle of the progress bar a click 'on' the progress bar can be, and still control the progress - should be at least progressWidth
+var progressClickWidth = 20;
+
 audio.controls = false;
 
 audio.addEventListener('timeupdate', function() {
   	updateProgress();
 }, false);
+
+document.getElementById('container').addEventListener('click', function(e){
+	var player = document.getElementById('player'),
+		//playerBounds is the rectangle encompassing the player
+		playerBounds = player.getBoundingClientRect(),
+		//xDistance is the distance on the x-axis of the click relative to the centre
+		xDistance = e.x - (playerBounds.left + (player.offsetWidth / 2)),
+		//yDistance is the same, but for the y-axis
+		yDistance = e.y - (playerBounds.top + (player.offsetHeight / 2)),
+		//distanceFromCentre is how far the click was from the centre of the player
+		distanceFromCentre = Math.sqrt((xDistance * xDistance) + (yDistance * yDistance)),
+		//we might use these later to work out the new progress percentage
+		clickAngle,
+		newProgressPercentage,
+		//and here's a handy constant
+		twoPi = 2 * Math.PI;
+			
+	//use distanceFromCentre to work out whether the progress bar was clicked - does it fall within the radius range?
+	if(distanceFromCentre >= progressRadius - progressClickWidth && distanceFromCentre <= progressRadius + progressClickWidth){
+		//note that although Math.atan2 is undefined for (0, 0) arguments, in practice the check above avoids that case given reasonable values
+		clickAngle = Math.atan2(yDistance, xDistance);
+		//account for the offset and make clickAngle positive - it currently ranges from -pi to pi, and progressOffsetRadians is at most twoPi, so we need to add twoPi twice before getting the remainder
+		clickAngle = (clickAngle + twoPi + twoPi - progressOffsetRadians) % twoPi;
+		//work out the percentage (0 to 1)
+		newProgressPercentage = clickAngle / twoPi
+		//probably over-cautious, but let's guard against rounding errors
+		if(newProgressPercentage < 0) newProgressPercentage = 0;
+		if(newProgressPercentage > 1) newProgressPercentage = 1;
+		//and set the new progress
+		audio.currentTime = audio.duration * newProgressPercentage;
+	}		
+});
 
 function togglePlayPause() {
    if (audio.paused || audio.ended) {
@@ -33,13 +74,20 @@ function updateProgress() {
 	var context = canvas.getContext('2d');
 	var centerX = canvas.width / 2;
 	var centerY = canvas.height / 2;
-	var radius = 150;
 	var circ = Math.PI * 2;
 	var quart = Math.PI / 2;
 	var cpercent = percent / 100; /* current percent */
+	//draw the clickable part of the progress bar
 	context.beginPath();
-	context.arc(centerX, centerY, radius, 0, ((circ) * cpercent), false);
-	context.lineWidth = 10;
+	context.arc(centerX, centerY, progressRadius, 0, circ, false);
+	context.lineWidth = progressClickWidth;
+	context.strokeStyle = '#f60';
+	context.stroke();
+	
+	//and draw the current progress
+	context.beginPath();
+	context.arc(centerX, centerY, progressRadius, progressOffsetRadians, progressOffsetRadians + ((circ) * cpercent), false);
+	context.lineWidth = progressWidth;
 	context.strokeStyle = '#ffc21c';
 	context.stroke();
 	if (audio.ended) resetPlayer();
@@ -55,11 +103,4 @@ function resetPlayer() {
 
 // Circular rotating music player with menu on back with html5 javascript and css3 is  MIT licensed:  http://opensource.org/licenses/MIT
 
-// modified for Knowitwall 2016:
-// this tests out the function `setLocation`: goes directly to the position at 80% of the song
-
-
-function setLocation(){
-    var percentage = 0.80;
-    audio.currentTime = audio.duration * percentage;
-}
+// modified for Knowitwall 2016
