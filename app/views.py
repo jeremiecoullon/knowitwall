2179bb803b815f4b056de4d9c303bdc7fcbf543b
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2016-02-22 10:18:47 +0000

    forgot to add blast_injury.mp3

--- app/views.py
+++ app/views.py
@@ -75,6 +75,7 @@ def send_file_partial(path):
 @app.route('/audio/real_shakespeare.mp3')
 @app.route('/audio/higgs_boson.mp3')
 @app.route('/audio/human_language.mp3')
+@app.route('/audio/blast_injury.mp3')
 def static_from_root():
     return send_file_partial(request.path[1:])
 
