23c7abbefaec36666301ab6d68fc947986c315f9
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2016-05-01 21:37:47 +0100

    ready to upload Alex' audiodoc

--- app/views.py
+++ app/views.py
@@ -90,6 +90,7 @@ def send_file_partial(path):
 @app.route('/audio/human_language.mp3')
 @app.route('/audio/blast_injury.mp3')
 @app.route('/audio/flying_spying.mp3')
+@app.route('/audio/right_to_travel.mp3')
 def static_from_root():
     return send_file_partial(request.path[1:])
 
