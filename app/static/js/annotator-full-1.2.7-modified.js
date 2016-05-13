a56e6d897b8ddcff7d8ac35a82c071627cca3f5b
Ian Jones <ianjones227@users.noreply.github.com>
2016-05-13 21:29:54 +0100

    Added kappa logo: 'For every annotation, place the small kappa logo at the end of the line'.

--- app/static/js/annotator-full-1.2.7-modified.js
+++ app/static/js/annotator-full-1.2.7-modified.js
@@ -147,6 +147,47 @@
   $ = jQuery;
 
   Util = {};
+  
+  //KiW modif: add Util.verticalAnchors
+  //Util.verticalAnchors manages elements which are to be vertically aligned. It exposes a register function,
+  //which accepts an anchor along with the element to align to it.
+  Util.verticalAnchors = (function(){
+    var registered = [];
+    
+    //updates the position of the element toAlign so that it is aligned with the anchor
+    function align(anchor, toAlign){
+        var anchorParent = anchor.parent();
+        toAlign.offset({
+            //the element toAlign should have its centre point vertically in line with that of the anchor
+            top: anchor.offset().top + ((anchor.height() - toAlign.height()) / 2),
+            //the element toAlign should have its left side a short distance from the right side of the anchor's parent
+            left: anchorParent.offset().left + anchorParent.width() + 5
+        });
+    }
+    
+    //when the window is resized the elements toAlign need to be realigned to their anchors.
+    $(window).on('resize', function(){
+        var i;
+        for(i = 0; i < registered.length; i += 1){
+            align(registered[i].anchor, registered[i].toAlign);
+        }
+    });
+    
+    return {
+        //aligns the element toAlign in relation to the provided anchor, and maintains their alignment
+        register: function(anchor, toAlign){
+            //the element toAlign must be absolutely positioned for the alignment to work
+            toAlign.css('position', 'absolute');
+            //set the initial alignment
+            align(anchor, toAlign);
+            //remember the pair
+            registered.push({
+                anchor: anchor,
+                toAlign: toAlign
+            });
+        }
+    };
+  })();
 
   Util.flatten = function(array) {
     var flatten;
@@ -765,7 +806,6 @@
       this.onEditAnnotation = __bind(this.onEditAnnotation, this);
       this.onAdderClick = __bind(this.onAdderClick, this);
       this.onAdderMousedown = __bind(this.onAdderMousedown, this);
-      this.knowit_button = __bind(this.knowit_button, this);
       this.toggleAnnotationViewer = __bind(this.toggleAnnotationViewer, this);
       this.onHighlightMouseover = __bind(this.onHighlightMouseover, this);
       this.checkForEndSelection = __bind(this.checkForEndSelection, this);
@@ -787,7 +827,6 @@
       this._setupWrapper()._setupViewer()._setupEditor();
       this._setupDynamicStyle();
       this.adder = $(this.html.adder).appendTo(this.wrapper).hide();
-      this.knowit_button = $(this.html.knowit_button).appendTo(this.wrapper).hide();
     }
 
     Annotator.prototype._setupWrapper = function() {
@@ -1003,7 +1042,8 @@
     };
 
     Annotator.prototype.highlightRange = function(normedRange, cssClass) {
-      var hl, node, white, _k, _len2, _ref1, _results;
+      //KiW modif: add kappa
+      var hl, node, white, _k, _len2, _ref1, _results, kappa;
       if (cssClass == null) {
         cssClass = 'annotator-hl';
       }
@@ -1017,6 +1057,10 @@
           _results.push($(node).wrapAll(hl).parent().show()[0]);
         }
       }
+      //KiW modif: anchor kappa vertically to the highlight
+      kappa = $(this.html.knowit_button);
+      $(node).parent().append(kappa);
+      Util.verticalAnchors.register($(node).parent(), kappa);
       return _results;
     };
 
@@ -1147,9 +1191,6 @@
       annotations = $(event.target).parents('.annotator-hl').andSelf().map(function() {
         return $(this).data("annotation");
       });
-      // show the knowit_button at the level of the mouse posiiton
-      this.knowit_button.css({"top": Util.annotationPosition(event, this.wrapper[0]).top,"right": "700px"});
-      this.knowit_button.show();
       return this.showViewer($.makeArray(annotations), Util.annotationPosition(event, this.wrapper[0]));
     };
 
