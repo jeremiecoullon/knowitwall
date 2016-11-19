984b47a038ffcfb407898208e34b6e40fa176792
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2016-11-19 14:09:51 +0400

    feedback message less robotic

--- app/static/js/contact_me.js
+++ app/static/js/contact_me.js
@@ -28,7 +28,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thanks for your feedback! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
