69fe19949f4689c8ddb74cbe967c9686451bd532
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2017-01-08 20:32:02 +0000

    another type

--- app/views.py
+++ app/views.py
@@ -209,7 +209,7 @@ def contactform():
     angus = 'anguswaite@gmail.com'
     subject = 'Knowitwall contact form, message by: '+str(name)
     body = "Le feedack! Here's their info: \n \n--------------------------------------------------------\naudiodoc name: \n"+ str(ad_name)+"\n\n \nname: " + str(name) + "\nemail: " + str(email) + "\n\n \n feedback: \n" + str(feedback_overall) + "\n\n \n--------------------------------------------------------"
-    yagmail.Connect('emailtoknowitwall', 'Ilovepasswords').send([jeremie], subject, body)
+    yagmail.Connect('emailtoknowitwall', 'Ilovepasswords').send([jeremie, miguel, KIW, angus], subject, body)
     return name
 
 
